4fd101ce00f15fd4017d9f2f75234e9db5a4218e
Levan Gelashvili <levan.gelashvili9@gmail.com>
2022-07-12 17:57:01 +0400

    Added random messages

---
+++ app.js
@@ -0,0 +1,9 @@
+const randNumber = (num) => {
+  Math.floor(Math.random() * num);
+};
+
+const randomIdeas = {
+  Addition: ["1 + 3 = 4", "4 + 7 = 11", "9 + 5 = 14", "3 + 6 = 9"],
+  Subtraction: ["7 - 3 = 4", "9 - 7 = 2", "13 - 4 = 9", "3 - 6 = -3"],
+  Multiplication: ["2 * 3 = 6", "4 * 5 = 20", "9 * 1 = 9", "7 * 6 = 42"],
+};
